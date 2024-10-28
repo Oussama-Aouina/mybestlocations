563dd1355ba2b4781802c999e1609f6e15c66c22
Oussama Aouina <oussamaaouina99@gmail.com>
2024-10-28 13:57:34 +0100

    you can add a position now

--- app/src/main/java/com/oussamaaouina/mybestlocation/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/oussamaaouina/mybestlocation/ui/slideshow/SlideshowFragment.java
@@ -1,16 +1,32 @@
 package com.oussamaaouina.mybestlocation.ui.slideshow;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.oussamaaouina.mybestlocation.Config;
+import com.oussamaaouina.mybestlocation.JSONParser;
+import com.oussamaaouina.mybestlocation.Position;
 import com.oussamaaouina.mybestlocation.databinding.FragmentSlideshowBinding;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+import kotlin.reflect.KParameter;
 
 public class SlideshowFragment extends Fragment {
 
@@ -22,9 +38,22 @@ public class SlideshowFragment extends Fragment {
 
         binding = FragmentSlideshowBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        Button add = binding.addBtn;
 
-        final TextView textView = binding.textSlideshow;
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HashMap<String,String> params = new HashMap<>();
 
+                params.put("longitude",binding.textLongitude.getText().toString());
+                params.put("latitude",binding.textLatitude.getText().toString());
+                params.put("numero",binding.textNumero.getText().toString());
+                params.put("pseudo",binding.textPseudo.getText().toString());
+
+                Upload u = new Upload(params);
+                u.execute();
+            }
+        });
         return root;
     }
 
@@ -32,5 +61,58 @@ public class SlideshowFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+    AlertDialog alert;
+
+    class Upload extends AsyncTask {
+        HashMap<String,String> params;
+        public Upload(HashMap<String,String> params) {
+            this.params = params;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            // UI Thread
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle("Upload");
+            builder.setMessage("Uploading...");
+            alert = builder.create();
+            alert.show();
+        }
+
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            // Code de thread secondaire (background)
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            // problem: pas d'acces a l'interface graphique
+            JSONParser parser = new JSONParser();
+            JSONObject response = parser.makeHttpRequest(Config.url_add,
+                    "POST",
+                    params);
+
+            try {
+                int success = response.getInt("success");
+                Log.e("response", "==" + success);
+                if(success == 1 ){
+                    Log.e("response", "==" + response);
+                }
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Object o) {
+            // UI Thread (Thread principal)
+            super.onPostExecute(o);
+            alert.dismiss();
+
+
+        }
     }
 }
